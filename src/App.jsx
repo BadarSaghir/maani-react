fe15ab5498eb4a3b752f4b8c1bab69f00d83248c
Sardar Badar <dataentrybadar@gmail.com>
2023-06-11 14:37:47 +0500

    added banner constant

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
 import style from './App.module.css'
 import {themes} from "./constant/theme"
+import {bannerConstants} from "./constant/banner"
+
 import { useContext, useEffect } from "react";
 import {ThemeContext} from "./contexts/themeContext"
+import Banner from "./components/Banner/Banner";
  const App = () => {
 //  const theme="" 
  const theme=useContext(ThemeContext)
@@ -15,6 +18,7 @@ useEffect(()=>{
   return (
     <div className={themes.dark===theme.theme ?style.darkMode:''}>
       <Navbar />
+      <Banner {...bannerConstants} />
     </div>
   );
 };

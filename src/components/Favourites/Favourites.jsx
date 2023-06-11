2a04cc83341732147965b34c54e9d8e17ba66088
Sardar Badar <dataentrybadar@gmail.com>
2023-06-11 22:29:03 +0500

    added minoe Changes

--- src/components/Favourites/Favourites.jsx
+++ src/components/Favourites/Favourites.jsx
@@ -1,11 +1,10 @@
 /* eslint-disable react/prop-types */
-import { useEffect } from "react"
 import FavouriteItem from "./FavouriteItem/FavouriteItem"
 import style from "./Favourites.module.css"
-export const Favourites = ({show=true})=>{
-useEffect(()=>{
-console.log("rerender")
-},[show])
+const Favourites = ({show=true})=>{
+// useEffect(()=>{
+// console.log("rerender")
+// },[show])
     return    <section className={`${ style["favourites-section"]} } `} id={`${show?"forward":
     "reverse"}`}>
     <div className={`${style["favourites-section-container"]} ${style["container"]}`}>
@@ -13,7 +12,7 @@ console.log("rerender")
             <h3>My Favourite Topics</h3>
             <div className={`${style["favourites-items-container"]}`}>
                 <FavouriteItem />
-                <FavouriteItem />
+                <FavouriteItem  />
 
             </div>
         </div>

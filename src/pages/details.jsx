c9154c884d2eee50e1c129d70d0de919d87e3a6f
Sardar Badar <dataentrybadar@gmail.com>
2023-06-12 20:21:56 +0500

    added main layout

--- src/pages/details.jsx
+++ src/pages/details.jsx
@@ -4,13 +4,14 @@ import { useSearchTopics } from "../hooks/useSearchTopics";
 import { tapWebApiGetDetailByIdParam } from "../constant/ApiEndPoints";
 import { SubTopicBox } from "../components/SubTopicBox/SubTopicBox";
 import { CourseDetailBox } from "../components/CourseDetailBox/CourseDetailBox";
+import style from "../styles/detail.module.css"
 const Details = () => {
   const { id } = useParams();
   const { data, isLoading } = useSearchTopics(tapWebApiGetDetailByIdParam(id));
   if (isLoading) return <div>Loading..</div>;
   return (
     <main>
-      <section id="details-page" className="details-page">
+      <section id="details-page" className={style['details-page']}>
         {<CourseDetailBox data={data} />}
         {<SubTopicBox data={data}/>}
       </section>

c92dc9cdebb734c9ae5ca29c2dbe345c20db2895
Sardar Badar <dataentrybadar@gmail.com>
2023-06-12 14:00:47 +0500

    added searchbar

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -3,7 +3,7 @@ import SearchBar from "./SearchInput/SearchBar"
 import styles from "./SearchBox.module.css"
 const SearchBox =()=>{
 
-    return <section id={styles['web-topics-page']} className={styles['web-topics-page']}>
+    return <section className={styles['web-topics-page']}>
    <SearchBar/>
    <ResultContainer/>
 </section>
--- src/components/SearchBox/SearchInput/SearchBar.jsx
+++ src/components/SearchBox/SearchInput/SearchBar.jsx
@@ -1,29 +1,67 @@
-const SearchBar =()=>{
-
-    return (<section id="search-bar-section" className="search-section">
-        <div className="container search-custom-container">
-            <div className="search-bar">
-                <span id="search-icon"><ion-icon class="search-icon" name="search-outline"></ion-icon></span>
-                <input className="search-field" type="search" id="SearchField" placeholder="Search the website..." />
-                <div className="search-sort-option search-options">
-                    <label id="search-sort-label" htmlFor="search-sort-select">Sort by:</label>
-                    <select id="search-sort-select" name="sort">
-                        <option value="Default">Default</option>
-                        <option value="Default">Topic Title</option>
-                        <option value="Default">Author Name</option>
-                    </select>
-                </div>
-                <div className="search-filter-option search-options d-flex flex-wrap border-start">
-                    <label id="search-filter-label" htmlFor="search-filter-select" className="w-100">Filter by:</label>
-                    <select id="search-filter-select" name="filter" className="w-100 border-0">
-                        <option value="Default">Default</option>
-                    </select>
-                </div>
-            </div>
+import style from "./SearchBar.module.css";
+const SearchBar = () => {
+  return (
+    <section
+   
+      className={`${style["search-section"]}`}
+    >
+      <div
+        className={` ${style["container"]}  ${style["search-custom-container"]}`}
+      >
+        <div className={`${style["search-bar"]}`}>
+          <span >
+        {/* TODO:ICON */}
+            {/* <ion-icon clas={`${style["search-icon"]}`}> name="search-outline"></ion-icon> */}
+          </span>
+          <input
+            className={`${style["search-field"]}`}
+            type="search"
+            
+            placeholder="Search the website..."
+          />
+       
+          <div
+            className={`${style["search-filter-option"]} ${style["search-options"]}  d-flex flex-wrap border-start`}
+          >
+            <label
+              id={`"search-filter-label"`}
+              htmlFor="search-filter-select"
+              className={`"w-100"`}
+            >
+              Sort by:
+            </label>
+            <select
+              id={`"search-filter-select"`}
+              name="filter"
+              className={`"w-100 border-0"`}
+            >
+              <option value="Default">Default</option>
+              <option value="Topic">Topic Title</option>
+              <option value="Author">Author Name</option>
+            </select>
+          </div>
+          <div
+            className={`${style["search-filter-option"]} ${style["search-options"]}  d-flex flex-wrap border-start`}
+          >
+            <label
+              id={`"search-filter-label"`}
+              htmlFor="search-filter-select"
+              className={`"w-100"`}
+            >
+              Filter by:
+            </label>
+            <select
+              id={`"search-filter-select"`}
+              name="filter"
+              className={`"w-100 border-0"`}
+            >
+              <option value="Default">Default</option>
+            </select>
+          </div>
         </div>
-    </section>)
-    
+      </div>
+    </section>
+  );
+};
 
-}
-
-export default SearchBar
+export default SearchBar;

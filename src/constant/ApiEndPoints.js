c72c25037eb819b0558af8dad40647c9e8245650
Sardar Badar <dataentrybadar@gmail.com>
2023-06-12 19:34:45 +0500

    handling loading data

--- src/constant/ApiEndPoints.js
+++ src/constant/ApiEndPoints.js
@@ -1,3 +1,5 @@
 export const tapWebApiEndPoints="https://tap-web-1.herokuapp.com/topics"
 export const tapWebApiListParam=()=> "/list"
 export const tapWebApiListPharaseParam=(pharase)=> "/list?phrase="+pharase
+export const tapWebApiGetDetailByIdParam=(id)=> "/details/"+id
+export default tapWebApiEndPoints
--- src/pages/details.jsx
+++ src/pages/details.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable react/prop-types */
 import {useParams} from "react-router-dom";
+import {useSearchTopics} from "../hooks/useSearchTopics"
+import {tapWebApiGetDetailByIdParam} from "../constant/ApiEndPoints"
+
 const  Details=()=>{
     const {id} = useParams();
-
+    const {data,isLoading}=useSearchTopics(tapWebApiGetDetailByIdParam(id))
+    if(isLoading) return <div>Loading..</div>
     return (
         <main >    
-            {id}
+            {JSON.stringify(data)}
         </main>
     )
 }

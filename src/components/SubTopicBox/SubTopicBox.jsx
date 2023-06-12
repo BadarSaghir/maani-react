928ad7a1694cca1075f92e12e2a52d74b094054d
Sardar Badar <dataentrybadar@gmail.com>
2023-06-12 20:28:48 +0500

    added styles

--- src/components/SubTopicBox/SubTopicBox.jsx
+++ src/components/SubTopicBox/SubTopicBox.jsx
@@ -1,24 +1,30 @@
-import {FaRegCheckCircle} from "react-icons/fa"
+import { FaRegCheckCircle } from "react-icons/fa";
+import style from "./SubTopicBox.module.css";
 /* eslint-disable react/prop-types */
-export function SubTopicBox({data}) {
-    return <div
-        id="details-page-sub-topics-container"
-        className="details-page-container  details-page-sub-topics-container"
+export function SubTopicBox({ data }) {
+  return (
+    <div
+      id="details-page-sub-topics-container"
+      className={`${style["details-page-container"]} ${style["details-page-sub-topics-container"]}`}
     >
-        <div id="course-sub-topics" className="course-sub-topics">
-            <ul className="course-sub-topics-list" title="course-sub-topics">
-                <li className="course-sub-topic">
-                    <h2>{data.topic} Sub Topics</h2>
-                </li>
-                {data.subtopics.map((subTopic, idx) => (
-                    <li key={idx} className="course-sub-topic">
-                    <span color="green">
-                      <FaRegCheckCircle color="green" />
-                      </span> 
-                        <h3>{subTopic}</h3>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    </div>;
+      <div id="course-sub-topics" className={`${style["course-sub-topics"]}`}>
+        <ul
+          className={`${style["course-sub-topics-list"]}`}
+          title="course-sub-topics"
+        >
+          <li className={`${style["course-sub-topic"]}`}>
+            <h2>{data.topic} Sub Topics</h2>
+          </li>
+          {data.subtopics.map((subTopic, idx) => (
+            <li key={idx} className={`${style["course-sub-topic"]}`}>
+              <span color="green">
+                <FaRegCheckCircle color="green" />
+              </span>
+              <h3>{subTopic}</h3>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
